9282f5dc730adf025ccb8ea7a019d9528a7c3c0a
Sean Hellum <seanhellum45@gmail.com>
2019-11-11 21:16:29 +0000

    edits

--- ChatBot.java
+++ ChatBot.java
@@ -1,38 +1,49 @@
 import java.util.Scanner;
 
-/**
- * Chat bot thing!
- */
-public class ChatBot {
-
+class ChatBot {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Hello I am an un-intelligent bot!: ");
-        boolean notDone = true;
-        while (notDone) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // controls whether the chat bot is still talking
+        boolean keepRunning = true;
+
+        // inital chat bot message
+        System.out.println("Hello!");
+
+        // a loop that keeps reading in what the user types in
+        // it then prints out a response from the chat bot
+        while (keepRunning) {
             System.out.print("input> ");
-            String userInput2 = input.nextLine();
-            userInput2 = userInput2.toLowerCase();
-            if (userInput2.equals("What wr")) {
-                System.out.println("I was programmed in java. I am done talking.");
-            } else if (userInput2.equals("bye")) {
-                System.out.println("Ok, Bye!");
-                return;
-            } else if (userInput2.length() == 0) {
-                System.out.println("I can't hear you!");
-            } else {
-                int randNum = (int) (Math.random() * 5) + 1;
-                if (randNum == 1) {
-                    System.out.println("Very interesting");
-                } else if (randNum == 2) {
-                    System.out.println("Cool!");
-                } else if (randNum == 3) {
-                    System.out.println("Ok!");
-                } else if (randNum == 4) {
-                    System.out.println("Got it!");
-                } else {
-                    System.out.println("Hmmm...");
-                }
+            String userSaid = sc.nextLine();
+            String userSaid2 = userSaid.toLowerCase();
+            String response = getResponse(userSaid2);
+            System.out.println(response);
+        }
+    }
+
+    // method that determines what the chat bot says
+    // based on the users input
+    public static String getResponse(String userInput) {
+        if (userInput.contains("programming")) {
+            return "I was programmed in Java. I am done talking.";
+        } else if (userInput.equals("bye")) {
+            return "Ok, Bye!";
+        } else if (userInput.length() == 0) {
+            return "I can't hear you!";
+        } else {
+            int randNum = (int) (Math.random() * 5) + 1;
+            switch(randNum) {
+                case 1:
+                    return "Very interesting";
+                case 2:
+                    return "Cool!";
+                case 3:
+                    return "Ok!";
+                case 4:
+                    return "Got it!";
+                default:
+                    return "Hmmm...";
             }
         }
     }
