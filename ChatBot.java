e382c9ae9133db9c150b4ba4f604c70c9fbcc747
Sean Hellum <seanhellum45@gmail.com>
2019-11-11 16:41:17 +0000

    update chatbot

--- ChatBot.java
+++ ChatBot.java
@@ -7,27 +7,22 @@ public class ChatBot {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Hello I am an un-intelligent bot! \nWho are you?: ");
-        String name = input.nextLine();
-        if (name.equals("")) {
-            askAgain();
-        }
-        System.out.println("Well hello " + name);
-        System.out.print("Well, " + name + " what would you like to talk about?: ");
+        System.out.println("Hello I am an un-intelligent bot!: ");
         boolean notDone = true;
         while (notDone) {
+            System.out.print("input> ");
             String userInput2 = input.nextLine();
             userInput2 = userInput2.toLowerCase();
-            if (userInput2.equals("programming")) {
+            if (userInput2.equals("What wr")) {
                 System.out.println("I was programmed in java. I am done talking.");
-            } else if (userInput2.equals("nothing")) {
+            } else if (userInput2.equals("bye")) {
                 System.out.println("Ok, Bye!");
                 return;
+            } else if (userInput2.length == 0) {
+                System.out.println("I can't hear you!");
+            } else {
+                System.out.println("Very interesting");
             }
         }
-    }
-
-    public static void askAgain() {
-        //
     }
 }
